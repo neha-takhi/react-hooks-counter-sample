7f1dc8bae2b401644bbf468b1b2a6c86bd723260
sakai-akinobu <sakai.akinobu@gmail.com>
2018-11-03 16:07:08 +0900

    Add IncrementButton, DecrementButton components.

---
+++ src/components/IncrementButton/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function IncremntButton({onClick}) {
+  return <button onClick={onClick}>+</button>
+}
--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,11 +1,17 @@
 import React, {useState} from 'react';
 
 import Counter from '../../components/Counter';
+import IncrementButton from '../../components/IncrementButton';
+import DecrementButton from '../../components/DecrementButton';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Counter value={count} />
+    <div>
+      <DecrementButton />
+      <Counter value={count} />
+      <IncrementButton />
+    </div>
   );
 }
